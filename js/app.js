47ea20fff90180dfc51522eb26550c4198e908de
System User <cami.tsuki@gmail.com>
2015-05-30 09:50:56 -0500

    finalización del dojo

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@ App = Ember.Application.create();
 //router map es donde se mapea todas las vistas y controladores
 App.Router.map(function() {
 	this.route('create', {path: '/create'});
+	this.route('index', {path: '/'});
 });
 
 App.CreateController = Ember.Controller.extend({
 	actions: {
 		create: function(){
 			var userParams = this.get("model");
+			$.ajax({
+				url: 'http://proyectosudea.hol.es/users',
+				type: 'POST',
+				data: JSON.stringify(userParams),
+				contentType: 'text/json'
+			});
 		}
 	}
 });
@@ -20,7 +27,24 @@ App.CreateRoute = Ember.Route.extend({
 	},
 	setupController: function(controller, model){
 		controller.set('model', model);
-		controller.set('tittle', 'Crear Nuevo Usuario');
+		controller.set('tittle', 'Crear un nuevo usuario');
 	}
 });
 
+App.IndexController = Ember.Controller.extend({
+	tittle: 'Lista de usuarios',
+	users: '',
+	actions: {
+		delete: function(user){
+
+		}
+	}
+});
+
+App.IndexRoute = Ember.Route.extend({
+	setupController: function(controller){
+		$.get('http://proyectosudea.hol.es/users', function(data){
+			controller.set('users', JSON.parse(data).users);
+		});
+	}
+});
